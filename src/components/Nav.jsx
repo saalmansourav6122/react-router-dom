08fac01d3b3d32222fdb237a58b72fe92cdebbe8
saalmansourav6122 <saalmansourav111@gmail.com>
2025-02-24 16:00:39 +0600

    isactive added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 function Nav() {
   return (
     <div className="nav-div">
       <h1 className="nav-h">React Route</h1>
       <ul>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/product">Product</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/product"
+          className={({ isActive }) => (isActive ? "active-link" : "")}
+        >
+          Product
+        </NavLink>
       </ul>
     </div>
   );
